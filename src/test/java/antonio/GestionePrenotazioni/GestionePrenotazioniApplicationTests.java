d07fd345a272dfe0f93daa7fdd9550303992c820
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-10-25 10:14:47 +0200

    CreazioneDB

---
+++ src/test/java/antonio/GestionePrenotazioni/GestionePrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package antonio.GestionePrenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionePrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
