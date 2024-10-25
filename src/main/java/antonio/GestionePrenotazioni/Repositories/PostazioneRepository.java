02852d44ec79f032fac23f2ae8fd194b232e1194
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-10-25 13:34:01 +0200

    Gestione delle repositories , implementando  in prenotazione dei boolean per vedere se le postazioni e gli utenti esistono e se ha una prenotazione in una determianta data , stesso principio per postazione . Iniziati a scrivere i service , Utente per la creazione di utenti tramite metodi save e PostazioneService per la ricerca degli edifici e la citta

--- src/main/java/antonio/GestionePrenotazioni/Repositories/PostazioneRepository.java
+++ src/main/java/antonio/GestionePrenotazioni/Repositories/PostazioneRepository.java
@@ -1,9 +1,14 @@
 package antonio.GestionePrenotazioni.Repositories;
 
 import antonio.GestionePrenotazioni.Entities.Postazione;
+import antonio.GestionePrenotazioni.Enums.TipoPostazione;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PostazioneRepository extends JpaRepository<Postazione, UUID> {
+
+    List<Postazione> findByTipoAndEdificio_Citta(TipoPostazione tipo, String citta);
+
 }
--- src/main/java/antonio/GestionePrenotazioni/Repositories/PrenotazioneRepository.java
+++ src/main/java/antonio/GestionePrenotazioni/Repositories/PrenotazioneRepository.java
@@ -1,10 +1,18 @@
 package antonio.GestionePrenotazioni.Repositories;
 
+import antonio.GestionePrenotazioni.Entities.Postazione;
 import antonio.GestionePrenotazioni.Entities.Prenotazione;
+import antonio.GestionePrenotazioni.Entities.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 
 public interface PrenotazioneRepository extends JpaRepository<Prenotazione, UUID> {
+
+    boolean existsByUtenteAndData(Utente utente, LocalDate data);
+
+    boolean existsByPostazioneAndData(Postazione postazione, LocalDate data);
+
 }
